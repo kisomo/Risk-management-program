c4ef870bb424171801e3f9b9c874214561908471
Madona Syombua <syombuamadona@gmail.com>
2017-12-06 14:16:59 -0600

    Update developers1.java

--- developers1.java
+++ developers1.java
@@ -8,6 +8,14 @@ public class developers1
   String major;
   double experience;
 
+ 
+  /**
+	* developers1 class takes 5 arguments which
+	* 
+	* @return 	<code>true</code> if the all are called 
+  *          <code>false</code> otherwise.
+	*/
+  
   public developers1(String firstName, String lastName, String degreeLevel, String major, double experience)
   {
     this.firstName = firstName;
