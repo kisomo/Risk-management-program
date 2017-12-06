3757a960da839e386dbe80c761078508c7369de7
Madona Syombua <syombuamadona@gmail.com>
2017-12-06 14:21:53 -0600

    Update manage.java

--- manage.java
+++ manage.java
@@ -3,6 +3,11 @@ package ML;
 
 public class manage
 {
+/*
+* @Main method 
+* calls class developers2 , validators and developers1
+*     <code>false</code> otherwise.
+*/
      public static void main(String[] args)
     {
      developers2 John = new developers2("John","Terry","PhD","Maths",3);
