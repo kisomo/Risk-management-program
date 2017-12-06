2539b8535af61f05973c5735a17584064ad46705
Madona Syombua <syombuamadona@gmail.com>
2017-12-06 14:18:52 -0600

    Update developers2.java

--- developers2.java
+++ developers2.java
@@ -1,4 +1,4 @@
-package ML;
+ package ML;
 
 public class developers2
 {
@@ -7,7 +7,12 @@ public class developers2
   String degreeLevel;
   String major;
   double experience;
-
+ /**
+	* class developers2 gives a status report 
+	* 
+	* @return 	<code>true</code> if are the arguments are taken 
+  *  <code>false</code> otherwise.
+	*/
   public developers2(String firstName, String lastName, String degreeLevel, String major, double experience)
   {
    this.firstName = firstName;
